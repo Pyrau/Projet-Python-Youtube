f8906d36ea3667f732142c60f297ebec6a39eff9
Julien Muller <jmredlight02@gmail.com>
2017-08-27 21:07:30 +0100

    Maj du fichier pour la modularité
    Il fonctionne presque. J'ai une exception avec pytube qui veut pas me DL et se bloque. A tester autrement

--- playlist_youtube_modul.py
+++ playlist_youtube_modul.py
@@ -9,102 +9,15 @@
 #
 # ##########################################
 
-from selenium import webdriver
-import pytube
-import ffmpy
 import os
 import time
 import csv
-from selenium.webdriver.chrome.options import Options
-
-# ##########################################
-#
-#           CONFIGURATION, SET UP
-#
-# ##########################################
+import pytube
+import ffmpy
 from config import *
+from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
 
-
-def check_new_url(ancient_url):
-    if driver.current_url != ancient_url:
-                ancient_url = driver.current_url
-                yt_url = driver.current_url
-                yt = pytube.YouTube(yt_url)
-                name = yt.filename
-    return name
-
-
-def convert(path_dl, name):
-    extension_in = '.3gp'
-    extension_out = '.mp3'
-    ff = ffmpy.FFmpeg(  # paramétrage de ffmpeg
-        inputs={path_dl + name + extension_in: None},
-        outputs={path_dl + name + extension_out: None}
-    )
-    print("Convertissement en musique en cours")
-    ff.run()
-    print("Convertissement Terminé")
-    os.remove(path_dl + name + extension_in)
-
-
-def check_if_in_BDD(liste_musique, name):
-    flag = 0
-    for musique in liste_musique:
-        if musique[0] == name:
-            print("musique :" + name + "dans la BDD")
-            flag = 1
-    return flag
-
-
-def download(flag, liste_musique, name, path_dl):
-    if flag == 0:
-        print("musique :" + name + "PAS dans la BDD")
-        liste_musique.append([name])
-
-        print("Téléchargement vidéo en cours")
-        yt.get('3gp', '240p').download(path_dl)
-        print("Téléchargement vidéo terminé")
-
-
-
-def main():
-    database_setup()
-    url_yt = 'https://accounts.google.com/ServiceLogin?passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Fnext%3D%252F%26feature%3Dsign_in_button%26app%3Ddesktop%26hl%3Des%26action_handle_signin%3Dtrue&hl=es&uilel=3&service=youtube'
-    run_connect_chrome(url_yt)
-
-    ancient_url = 'o'
-    while 1 > 0:
-        try:
-            pytube.YouTube(driver.current_url).get_videos()
-        except:
-            time.sleep(1)
-        else:
-            check_new_url(ancient_url)
-            check_if_in_BDD(liste_musique, name)
-            download(flag, liste_musique, name, path_dl)
-            convert(path_dl, name)
-            wr = csv.writer(f, dialect='excel')
-
-            # on écrit le dernier élément de la liste: l'élément [-1].
-            # A SAVOIR!! le rédacteur écrit sur la ligne i avec i numéro de l'élément dans la liste.
-            # Si je veux juste ajouter un élément en derniere ligne sans l'entrer comme élément i de ma liste, je ne sais pas faire
-            wr.writerow(liste_musique[-1])
-            print("Musique ajouté dans la BDD")
-            f.flush()
-            time.sleep(5)
-    return
-
-
-def database_setup(path_BDD):
-    if os.path.isfile(path_BDD):
-        f = open(path_BDD, 'r+', newline='')
-        reader = csv.reader(f, dialect='excel')
-        liste_musique = list(reader)
-
-    else:
-        liste_musique = []
-        f = open(path_BDD, 'w', newline='')
-    return liste_musique
 
 
 def run_connect_chrome(url_yt):
@@ -128,7 +41,91 @@ def run_connect_chrome(url_yt):
         '.RveJvd.snByac')                                               # Field : Next
     element.click()
     print("Connected to Google Chrome")
+    return driver
 
 
-if __name__ == '__main__':
-    main()
+def check_new_url(ancient_url, driver):
+    print(driver.current_url)
+    if driver.current_url != ancient_url:
+        ancient_url = driver.current_url
+        yt_url = driver.current_url
+        yt = pytube.YouTube(yt_url)
+        name = yt.filename
+    return name, yt
+
+
+def convert(path_dl, name):
+    extension_in = '.3gp'
+    extension_out = '.mp3'
+    ff = ffmpy.FFmpeg(
+        inputs={path_dl + name + extension_in: None},
+        outputs={path_dl + name + extension_out: None}
+    )
+    print("Convertissement en musique en cours")
+    ff.run()
+    print("Convertissement Terminé")
+    os.remove(path_dl + name + extension_in)
+
+
+def check_if_in_BDD(liste_musique, name):
+    flag = 0
+    for musique in liste_musique:
+        if musique[0] == name:
+            print("musique :" + name + "dans la BDD")
+            flag = 1
+    return flag
+
+
+def download(flag, liste_musique, name, path_dl, yt):
+    if flag == 0:
+        print("musique :" + name + "PAS dans la BDD")
+        liste_musique.append([name])
+
+        print("Téléchargement vidéo en cours")
+        yt.get('3gp', '240p').download(path_dl)
+        print("Téléchargement vidéo terminé")
+    return liste_musique
+
+
+def database_setup(path_BDD):
+    if os.path.isfile(path_BDD):
+        f = open(path_BDD, 'r+', newline='')
+        reader = csv.reader(f, dialect='excel')
+        liste_musique = list(reader)
+
+    else:
+        liste_musique = []
+        f = open(path_BDD, 'w', newline='')
+    return liste_musique, f
+
+
+def main():
+    (liste_musique, f) = database_setup(path_BDD)
+    url_yt = 'https://accounts.google.com/ServiceLogin?passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Fnext%3D%252F%26feature%3Dsign_in_button%26app%3Ddesktop%26hl%3Des%26action_handle_signin%3Dtrue&hl=es&uilel=3&service=youtube'
+    driver = run_connect_chrome(url_yt)
+
+    ancient_url = 'o'
+    while 1 > 0:
+        try:
+            pytube.YouTube(driver.current_url).get_videos()
+        except:
+            time.sleep(1)
+        else:
+            (name, yt) = check_new_url(ancient_url, driver)
+            flag = check_if_in_BDD(liste_musique, name)
+            if flag is False:
+                liste_musique = download(flag, liste_musique, name, path_dl, yt)
+                convert(path_dl, name)
+                wr = csv.writer(f, dialect='excel')
+
+                # on écrit le dernier élément de la liste: l'élément [-1].
+                # A SAVOIR!! le rédacteur écrit sur la ligne i avec i numéro de l'élément dans la liste.
+                # Si je veux juste ajouter un élément en derniere ligne sans l'entrer comme élément i de ma liste, je ne sais pas faire
+                wr.writerow(liste_musique[-1])
+                print("Musique ajouté dans la BDD")
+                f.flush()
+                time.sleep(5)
+    return
+
+
+main()
